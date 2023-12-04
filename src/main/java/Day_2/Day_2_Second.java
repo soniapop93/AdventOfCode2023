30bf3c20cdad735963d9393c526ac3e2a9ba1db7
soniapop93 <popsonia85@gmail.com>
2023-12-04 22:27:45 +0200

    finished day 2, part 2

---
+++ src/main/java/Day_2/Day_2_Second.java
@@ -0,0 +1,54 @@
+package Day_2;
+
+public class Day_2_Second {
+    public Integer solution(String input) {
+        String[] inputSplitted = input.split("\n");
+
+        Integer result = 0;
+
+        for (int i = 0; i < inputSplitted.length; i++) {
+            Integer gameNumber = Integer.valueOf(inputSplitted[i].split(":")[0].replace("Game ", ""));
+
+            String[] sets = inputSplitted[i].replace(("Game " + gameNumber.toString() + ": "), "").split(";");
+
+            Integer redCube = -1;
+            Integer blueCube = -1;
+            Integer greenCube = -1;
+
+            for (String set : sets) {
+                String[] cubes = set.split(",");
+
+                for (String cube : cubes) {
+                    if (cube.contains("red")) {
+                        Integer redValue = Integer.valueOf(cube.replace(" ","").replace("red", ""));
+
+                        if (redValue > redCube) {
+                            redCube = redValue;
+                        }
+                    }
+                    else if (cube.contains("blue")) {
+                        Integer blueValue = Integer.valueOf(cube.replace(" ","").replace("blue", ""));
+
+                        if (blueValue > blueCube) {
+                            blueCube = blueValue;
+                        }
+
+                    }
+                    else if (cube.contains("green")) {
+                        Integer greenValue = Integer.valueOf(cube.replace(" ","").replace("green", ""));
+
+                        if (greenValue > greenCube) {
+                            greenCube = greenValue;
+                        }
+                    }
+                }
+            }
+
+            Integer value = Math.abs(redCube * greenCube * blueCube);
+
+            result += value;
+        }
+
+        return result;
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 import Day_1.Day_1_First;
 import Day_1.Day_1_Second;
 import Day_2.Day_2_First;
+import Day_2.Day_2_Second;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -11,18 +12,22 @@ public class Main {
         String input = readFile("Inputs\\Input.txt");
 
         // Day 1
-//        Day_1_First day1First = new Day_1_First();
-//        System.out.println(day1First.solution(inputTest));
-//        System.out.println(day1First.solution(input));
-//
-//        Day_1_Second day1Second = new Day_1_Second();
-//        System.out.println(day1Second.solution(inputTest));
-//        System.out.println(day1Second.solution(input));
+        Day_1_First day1First = new Day_1_First();
+        System.out.println(day1First.solution(inputTest));
+        System.out.println(day1First.solution(input));
+
+        Day_1_Second day1Second = new Day_1_Second();
+        System.out.println(day1Second.solution(inputTest));
+        System.out.println(day1Second.solution(input));
 
         // Day 2
         Day_2_First day2First = new Day_2_First();
         System.out.println(day2First.solution(inputTest));
         System.out.println(day2First.solution(input));
+
+        Day_2_Second day2Second = new Day_2_Second();
+        System.out.println(day2Second.solution(inputTest));
+        System.out.println(day2Second.solution(input));
     }
 
     private static String readFile(String file) {
