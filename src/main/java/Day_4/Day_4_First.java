59ac4c7c9b9bd59aa1e65f2ef28f50b36f98806c
soniapop93 <popsonia85@gmail.com>
2023-12-05 23:34:26 +0200

    finished day 4, part 1

--- src/main/java/Day_4/Day_4_First.java
+++ src/main/java/Day_4/Day_4_First.java
@@ -4,23 +4,38 @@ import java.util.Arrays;
 
 public class Day_4_First {
     public Integer solution(String input) {
-        Integer result = 0;
+        int result = 0;
 
+        input = input.replace("  ", " ");
         String[] splittedInput = input.split("\n");
 
         for (int i = 0; i < splittedInput.length; i++){
-            String[] numbersSplitted = splittedInput[i].split(":")[1].trim().split("|");
+            String[] numbersSplitted = splittedInput[i].split(":")[1].trim().split("\\|");
 
             String[] winningNumbers = numbersSplitted[0].split(" ");
             String[] myNumbers = numbersSplitted[1].split(" ");
 
-            Integer points = 0;
+            int winningPoints = 0;
 
             for (int j = 0; j < myNumbers.length; j++) {
                 if (Arrays.asList(winningNumbers).contains(myNumbers[j])) {
-                    //todo: finish it
+                    winningPoints++;
                 }
             }
+
+            int points = 0;
+
+            if (winningPoints >= 1) {
+                winningPoints--;
+                points = 1;
+            }
+            while (winningPoints > 0) {
+                points *= 2;
+
+                winningPoints--;
+            }
+
+            result += points;
         }
 
         return result;
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ import Day_1.Day_1_First;
 import Day_1.Day_1_Second;
 import Day_2.Day_2_First;
 import Day_2.Day_2_Second;
+import Day_4.Day_4_First;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -12,22 +13,27 @@ public class Main {
         String input = readFile("Inputs\\Input.txt");
 
         // Day 1
-        Day_1_First day1First = new Day_1_First();
-        System.out.println(day1First.solution(inputTest));
-        System.out.println(day1First.solution(input));
-
-        Day_1_Second day1Second = new Day_1_Second();
-        System.out.println(day1Second.solution(inputTest));
-        System.out.println(day1Second.solution(input));
-
-        // Day 2
-        Day_2_First day2First = new Day_2_First();
-        System.out.println(day2First.solution(inputTest));
-        System.out.println(day2First.solution(input));
-
-        Day_2_Second day2Second = new Day_2_Second();
-        System.out.println(day2Second.solution(inputTest));
-        System.out.println(day2Second.solution(input));
+//        Day_1_First day1First = new Day_1_First();
+//        System.out.println(day1First.solution(inputTest));
+//        System.out.println(day1First.solution(input));
+//
+//        Day_1_Second day1Second = new Day_1_Second();
+//        System.out.println(day1Second.solution(inputTest));
+//        System.out.println(day1Second.solution(input));
+//
+//        // Day 2
+//        Day_2_First day2First = new Day_2_First();
+//        System.out.println(day2First.solution(inputTest));
+//        System.out.println(day2First.solution(input));
+//
+//        Day_2_Second day2Second = new Day_2_Second();
+//        System.out.println(day2Second.solution(inputTest));
+//        System.out.println(day2Second.solution(input));
+//
+        // Day 4
+        Day_4_First day4First = new Day_4_First();
+        System.out.println(day4First.solution(inputTest));
+        System.out.println(day4First.solution(input));
     }
 
     private static String readFile(String file) {
